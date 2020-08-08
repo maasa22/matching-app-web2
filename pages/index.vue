e682ba8d635e35bcf256b82b78ac5a45b5b082a9
masaki <maa22sa@gmail.com>
2020-08-08 17:28:45 +0900

    try firestore

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,17 @@
 <template>
   <div class="container">
-    <header class="header">
-      <h1>Chat</h1>
-      <!-- ログイン時にはフォームとログアウトボタンを表示 -->
-      <div v-if="user.uid" key="login">
-        [{{ user.displayName }}]
-        <button type="button" @click="doLogout">ログアウト</button>
-      </div>
-      <!-- 未ログイン時にはログインボタンを表示 -->
-      <div v-else key="logout">
-        <button type="button" @click="doLogin">ログイン</button>
-      </div>
-    </header>
-
-    <!--　Firebase から取得したリストを描画（トランジション付き）　-->
-    <!-- <transition-group name="chat" tag="div" class="list content"> -->
-
-    <section
-      v-for="{ key, name, image, message } in chat"
-      :key="key"
-      class="item"
-    >
-      <div class="item-image">
-        <img :src="image" width="40" height="40" />
-      </div>
-      <div class="item-detail">
-        <div class="item-name">{{ name }}</div>
-        <div class="item-message">
-          <p>{{ message }}</p>
-        </div>
-      </div>
-    </section>
-    <!-- </transition-group> -->
-
-    <!-- 入力フォーム -->
-    <form action @submit.prevent="doSend" class="form">
-      <textarea
-        v-model="input"
-        :disabled="!user.uid"
-        @keydown.enter.exact.prevent="doSend"
-      ></textarea>
-      <button type="submit" :disabled="!user.uid" class="send-button">
-        Send
-      </button>
-    </form>
-    <!-- <p>hello</p>
-    <p>{{ hoge }}</p>-->
-    <!-- <p class="title is-1 is-spaced">user: {{ $store.getters }}</p> -->
-    <!-- <button class="button is-primary is-rounded" @click="login">
+    <p class="title is-1 is-spaced">user: {{ $store.getters.getUserName }}</p>
+    <button class="button is-primary is-rounded" @click="login">
       ログイン
-    </button>-->
-
-    <!-- <table class="table is-narrow">
-      <thead>
+    </button>
+    <!-- <p>{{ todosA }}</p> -->
+    <table class="table is-narrow">
+      <tbody>
         <tr>
           <th>todo</th>
-
           <th>limit</th>
         </tr>
-      </thead>
-
+      </tbody>
       <tbody>
         <tr v-for="todo in $store.getters.getTodos" :key="todo.todo">
           <td>{{ todo.todo }}</td>
@@ -67,10 +19,12 @@
         </tr>
       </tbody>
     </table>
-    <div class="field is-grouped" style="background-color:#222222;">
+
+    <div class="field is-grouped">
       <p class="control is-expanded">
         <input v-model="newTodo" class="input" type="text" placeholder="todo" />
       </p>
+
       <p class="control is-expanded">
         <input
           v-model="newLimit"
@@ -79,344 +33,67 @@
           placeholder="limit"
         />
       </p>
+
       <p class="control">
-        <a class="button is-primary" @click="addTodo">
-          add
-        </a>
+        <a class="button is-primary" @click="addTodo"> add </a>
       </p>
-    </div>-->
-    <!-- <button @click="checkLogin">check login</button> -->
+    </div>
   </div>
-  <!-- <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>-->
 </template>
 
 <script>
-// import Logo from "~/components/Logo.vue";
-// import VuetifyLogo from "~/components/VuetifyLogo.vue";
-import firebase from "~/plugins/firebase";
-import "firebase/firestore";
-
 export default {
-  // components: {
-  //   Logo,
-  //   VuetifyLogo
-  // },
   data() {
     return {
-      hoge: "aaa",
-      // newTodo: "",
-      // newLimit: ""
-      user: {}, // ユーザー情報
-      chat: [], // 取得したメッセージを入れる配列
-      input: "" // 入力したメッセージ
-      // messages_firestore: []
+      newTodo: "",
+      newLimit: ""
     };
   },
-  // methods: {
-  // login() {
-  //   console.log("login");
-  //   this.$store.dispatch("login");
-  // },
-  // addTodo() {
-  //   const todo = this.newTodo;
-  //   const limit = this.newLimit;
-  //   this.$store.dispatch("addTodo", { todo, limit });
-  //   this.newTodo = "";
-  //   this.newLimit = "";
-  // },
-  // checkLogin() {
-  //   var user = firebase.auth().currentUser;
-  //   console.log(user);
-  // }
+  methods: {
+    login() {
+      console.log("login");
+      this.$store.dispatch("login"); //index.jsのactionsで定義した、login関数を実行しているだけ。
+    },
+    addTodo() {
+      const doc = firebase.firestore().collection("todos").doc;
+      const observer = doc.onSnapshot(
+        docSnapshot => {
+          console.log(`Received doc snapshot: ${docSnapshot}`);
+          // ...
+        },
+        err => {
+          console.log(`Encountered error: ${err}`);
+        }
+      );
+
+      const todo = this.newTodo;
+      const limit = this.newLimit;
+
+      this.$store.dispatch("addTodo", { todo, limit });
+      this.newTodo = "";
+      this.newLimit = "";
+    }
+  },
+  // computed: {
+  //   todosA() {
+  //     return this.$store.dispatch("fetchTodos");
+  //   }
   // },
   created() {
-    console.log(this.$store.state.messages);
-    firebase.auth().onAuthStateChanged(user => {
-      this.user = user ? user : {};
-      // const ref_message = firebase.database().ref("message");
-      // const db = firebase.firestore();
-      // const ref_message = db.collection("message");
-      // console.log(ref_message);
-      if (user) {
-        this.chat = [];
-        const db = firebase.firestore();
-        let citiesRef = db.collection("message");
-        let allCities = citiesRef
-          .get()
-          .then(snapshot => {
-            snapshot.forEach(doc => {
-              console.log(doc.id, "=>", doc.data());
-              this.chat.push(doc.data());
-            });
-          })
-          .catch(err => {
-            console.log("Error getting documents", err);
-          });
-        // message に変更があったときのハンドラを登録
-        // ref_message.limitToLast(10).on("child_added", this.childAdded);
-      } else {
-        // message に変更があったときのハンドラを解除
-        // ref_message.limitToLast(10).off("child_added", this.childAdded);
-      }
-    });
-  },
-  // firestore() {
-  //   return {
-  //     // firestoreのcommentsコレクションを参照
-  //     messages_firestore: firebase
-  //       .firestore()
-  //       .collection("message")
-  //       .orderBy("message")
-  //   };
-  // },
-  methods: {
-    // ログイン処理
-    doLogin() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider);
-    },
-    // ログアウト処理
-    doLogout() {
-      firebase.auth().signOut();
-    },
-    // スクロール位置を一番下に移動
-    scrollBottom() {
-      this.$nextTick(() => {
-        window.scrollTo(0, document.body.clientHeight);
-      });
-    },
-    // 受け取ったメッセージをchatに追加
-    // データベースに新しい要素が追加されると随時呼び出される
-    childAdded(snap) {
-      const message = snap.val();
-      this.chat.push({
-        key: snap.key,
-        name: message.name,
-        image: message.image,
-        message: message.message
-      });
-      this.scrollBottom();
-    },
-    async doSend() {
-      if (this.user.uid && this.input.length) {
-        // firebase にメッセージを追加
-        const res = await firebase
-          .firestore()
-          .collection("message")
-          .add({
-            message: this.input,
-            name: this.user.displayName,
-            image: this.user.photoURL
-          });
-        this.chat.push({
-          message: this.input,
-          name: this.user.displayName,
-          image: this.user.photoURL
-        });
-        this.input = ""; // フォームを空にする
-      }
-    }
+    this.$store.dispatch("fetchTodos");
   }
-  // computed: {
-  //   computedChat: function() {
-  //     return firebase.firestore().collection("message");
-  //   }
-  // }
-  // created() {
-  //   firebase.auth().onAuthStateChanged(function(user) {
-  //     if (user) {
-  //       console.log("ok-");
-  //       // $nuxt.$router.push("/inspire");
-  //       this.hoge = user.displayName;
-  //       this.hoge = "dddd ";
-  //       console.log(user);
-  //       console.log(user.displayName);
-  //     } else {
-  //       console.log("nook-");
-  //     }
-  //   });
-  // }
-  // mounted() {
-  //   firebase.auth().onAuthStateChanged(function(user) {
-  //     if (user) {
-  //       console.log("ok");
-  //       // $nuxt.$router.push("/inspire");
-  //     } else {
-  //       console.log("nook");
-  //       $nuxt.$router.push("/login");
-  //     }
-  //   });
-  //   // this.$store.dispatch("fetchTodos");
-  //   // console.log(this.$store.getters.getTodos);
-  // }
 };
 </script>
-<style>
-/* .container {
-  background-color: #aaaaaa;
-} */
 
-* {
-  margin: 0;
-  box-sizing: border-box;
-}
-.header {
-  background: #3ab383;
-  margin-bottom: 1em;
-  padding: 0.4em 0.8em;
-  color: #fff;
-}
-.content {
+<style>
+.container {
   margin: 0 auto;
-  padding: 0 10px;
-  max-width: 600px;
-}
-.form {
-  position: fixed;
+  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  bottom: 0;
-  height: 80px;
-  width: 80%;
-  background: #f5f5f5;
-  margin-bottom: 50px;
-}
-.form textarea {
-  border: 1px solid #ccc;
-  border-radius: 2px;
-  height: 4em;
-  width: calc(100% - 6em);
-  resize: none;
-}
-.list {
-  margin-bottom: 100px;
-}
-.item {
-  position: relative;
-  display: flex;
-  align-items: flex-end;
-  margin-bottom: 0.8em;
-}
-.item-image img {
-  border-radius: 20px;
-  vertical-align: top;
-}
-.item-detail {
-  margin: 0 0 0 1.4em;
-}
-.item-name {
-  font-size: 75%;
-}
-.item-message {
-  position: relative;
-  display: inline-block;
-  padding: 0.8em;
-  background: #113322;
-  border-radius: 4px;
-  line-height: 1.2em;
-}
-.item-message::before {
-  position: absolute;
-  content: " ";
-  display: block;
-  left: -16px;
-  bottom: 12px;
-  border: 4px solid transparent;
-  border-right: 12px solid #deefe8;
-}
-.send-button {
-  height: 4em;
-  background-color: #339933;
-}
-/* トランジション用スタイル */
-.chat-enter-active {
-  transition: all 1s;
-}
-.chat-enter {
-  opacity: 0;
-  transform: translateX(-1em);
+  text-align: center;
+  flex-direction: column;
+  background-color: #888888;
 }
 </style>
--- store/index.js
+++ store/index.js
@@ -1,97 +1,125 @@
 import firebase from "~/plugins/firebase";
-// const db = firebase.firestore();
-// const todoRef = db.collection("todos");
+
+const db = firebase.firestore();
+const todoRef = db.collection("todos");
+
 export const state = () => ({
-  messages: {
-    message: "hoge",
-    name: "hoge",
-    image: "https://via.placeholder.com/200x100"
-  }
+  userUid: "",
+  userName: "",
+  todos: []
 });
 
+// todoRef.orderBy("todo").onSnapshot(snapshot => {
+//   let changes = snapshot.docChanges();
+//   changes.forEach(change => {
+//     if ((change.type = "added")) {
+//       console.log("added", change.doc.id, change.doc.data());
+//       //   state.todos.push(change.doc.data());
+//     } else if (change.type == "removed") {
+//       console.log("removed", change.doc.id, change.doc.data());
+//     }
+//   });
+//   //   console.log(changes);
+// });
+
 export const mutations = {
-  addMessage(state, message) {
-    state.messages.push(message);
+  setUserUid(state, userUid) {
+    state.userUid = userUid;
+  },
+  setUserName(state, userName) {
+    state.userName = userName;
+  },
+  addTodo(state, todo) {
+    state.todos.push(todo);
+  },
+  //   deleteTodo(state, id) {
+  //     state.todos.push(todo);
+  //   },
+  clearTodo(state) {
+    state.todo = [];
   }
 };
 
-// export const state = () => ({
-//   userUid: "",
-//   userName: "",
-//   todos: []
-// });
+export const actions = {
+  login({ commit }) {
+    console.log("login action");
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        const user = result.user;
+        // console.log("success : " + user.uid + " : " + user.displayName);
+        commit("setUserUid", user.uid);
+        commit("setUserName", user.displayName);
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        // console.log("error : " + errorCode);
+      });
+  },
+  fetchTodos({ commit }) {
+    todoRef
+      //   .get()
+      //   .then(res => {
+      //     res.forEach(doc => {
+      //       //   console.log("success : " + `${doc.id} => ${doc.data()}`);
+      //       commit("addTodo", doc.data());
+      //     });
+      //   })
+      //   .catch(error => {
+      //     // console.log("error : " + error);
+      //   })
+      .orderBy("todo")
+      .onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+          if ((change.type = "added")) {
+            console.log("added", change.doc.id, change.doc.data());
+            commit("addTodo", change.doc.data());
+            //   state.todos.push(change.doc.data());
+          } else if (change.type == "removed") {
+            console.log("removed", change.doc.id, change.doc.data());
+            // commit("deleteTodo", change.doc.id);
+          }
+        });
+        //   console.log(changes);
+      });
+  },
+  addTodo({ commit }, todo) {
+    // console.log(todo);
+    todoRef
+      .add({
+        todo: todo.todo,
+        limit: todo.limit
+      })
+      .then(function(docRef) {
+        // console.log("Document written with ID: ", docRef.id);
+        commit("addTodo", todo);
+      })
+      .catch(function(error) {
+        // console.error("Error adding document: ", error);
+      });
+  }
+  //   deleteTodo({ commit }, id) {
+  //     todoRef.doc(id).delete();
+  //     then(function(docRef) {
+  //       // console.log("Document written with ID: ", docRef.id);
+  //       commit("deleteTodo", todo);
+  //     }).catch(function(error) {
+  //       // console.error("Error adding document: ", error);
+  //     });
+  //   }
+};
 
-// export const mutations = {
-//   setUserUid(state, userUid) {
-//     state.userUid = userUid;
-//   },
-//   setUserName(state, userName) {
-//     state.userName = userName;
-//   },
-//   addTodo(state, todo) {
-//     state.todos.push(todo);
-//   },
-//   clearTodo(state) {
-//     state.todo = [];
-//   }
-// };
-
-// export const actions = {
-//   login({ commit }) {
-//     console.log("login action");
-//     const provider = new firebase.auth.GoogleAuthProvider();
-//     firebase
-//       .auth()
-//       .signInWithPopup(provider)
-//       .then(function(result) {
-//         const user = result.user;
-//         console.log("success : " + user.uid + " : " + user.displayName);
-//         commit("setUserUid", user.uid);
-//         commit("setUserName", user.displayName);
-//       })
-//       .catch(function(error) {
-//         var errorCode = error.code;
-//         console.log("error : " + errorCode);
-//       });
-//   },
-//   fetchTodos({ commit }) {
-//     todoRef
-//       .get()
-//       .then(res => {
-//         res.forEach(doc => {
-//           console.log("success : " + `${doc.id} => ${doc.data()}`);
-//           commit("addTodo", doc.data());
-//         });
-//       })
-//       .catch(error => {
-//         console.log("error : " + error);
-//       });
-//   },
-//   addTodo({ commit }, todo) {
-//     console.log(todo);
-//     todoRef
-//       .add({
-//         todo: todo.todo,
-//         limit: todo.limit
-//       })
-//       .then(function(docRef) {
-//         console.log("Document written with ID: ", docRef.id);
-//         commit("addTodo", todo);
-//       })
-//       .catch(function(error) {
-//         console.error("Error adding document: ", error);
-//       });
-//   }
-// };
-
-// export const getters = {
-//   getUserUid(state) {
-//     return state.userUid;
-//   },
-//   getUserName(state) {
-//     return state.userName;
-//   },
-//   getTodos(state) {
-//     return state.todos;
-//   }
-// };
+export const getters = {
+  getUserUid(state) {
+    return state.userUid;
+  },
+  getUserName(state) {
+    return state.userName;
+  },
+  getTodos(state) {
+    return state.todos;
+  }
+};
