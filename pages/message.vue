b8f94fc31faeb45a423a1002ca54f038ab87fb7b
masaki <maa22sa@gmail.com>
2020-09-13 22:58:03 +0900

    mod

--- pages/message.vue
+++ pages/message.vue
@@ -10,21 +10,26 @@
       <div v-else>
         <p>{{ user.email }}でログイン中</p>
         <button @click="logOut">ログアウト</button>
-        <p class="title is-1 is-spaced">
-          user: {{ $store.getters.getUserName }}
-        </p>
+        <p>{{ messages_filtered }}</p>
+        <p class="title is-1 is-spaced">user: {{ $store.getters.getUserName }}</p>
         <table class="table is-narrow">
           <tbody>
             <tr>
+              <th>receiver</th>
+              <th>sender</th>
+              <th>createdAt</th>
               <th>message</th>
+              <th>message_id</th>
             </tr>
           </tbody>
           <tbody>
-            <tr
-              v-for="message in $store.getters.getmessages"
-              :key="message.message"
-            >
-              <td>{{ message }}</td>
+            <tr v-for="message in $store.getters.getmessages" :key="message.message">
+              <!-- <td>{{ message }}</td> -->
+              <td>{{ message.receiver }}</td>
+              <td>{{ message.sender }}</td>
+              <td>{{ message.createdAt }}</td>
+              <td>{{ message.message}}</td>
+              <td>{{ message.message_id }}</td>
               <!-- <td>{{ message.email }}</td> -->
             </tr>
           </tbody>
@@ -32,12 +37,7 @@
 
         <div class="field is-grouped">
           <p class="control is-expanded">
-            <input
-              v-model="newmessage"
-              class="input"
-              type="text"
-              placeholder="message"
-            />
+            <input v-model="newmessage" class="input" type="text" placeholder="message" />
           </p>
 
           <!-- <p class="control is-expanded">
@@ -47,10 +47,10 @@
               type="text"
               placeholder="email"
             />
-          </p> -->
+          </p>-->
 
           <p class="control">
-            <a class="button is-primary" @click="addmessage"> add </a>
+            <a class="button is-primary" @click="addmessage">add</a>
           </p>
         </div>
       </div>
@@ -69,14 +69,15 @@ export default {
       user: [],
       newmessage: "",
       newreceiver: "hoge@gmail.com",
-      newsender: "hoge@gmail.com"
+      newsender: "hoge@gmail.com",
+      messages: [],
       // newemail: ""
     };
   },
   // mounted: function() {
   created() {
     // this.$store.state.messages = [];
-    firebase.auth().onAuthStateChanged(user => {
+    firebase.auth().onAuthStateChanged((user) => {
       this.isWaiting = false;
       if (user) {
         this.isLogin = true;
@@ -95,6 +96,13 @@ export default {
     },
     logOut() {
       firebase.auth().signOut();
+    },
+    getMatchedPartners() {
+      let matchedPartners = firebase
+        .firestore()
+        .collection("likes")
+        .where("receiver", "==", this.loginUserGoogle.email)
+        .where("mail", "==", this.loginUserGoogle.email);
     },
     addmessage() {
       // const doc = firebase.firestore().collection("messages").doc;
@@ -129,12 +137,37 @@ export default {
         message,
         sender,
         receiver,
-        createdAt
+        createdAt,
       });
       this.newmessage = "";
       // this.newemail = "";
-    }
-  }
+    },
+    findBy: function (list, value, column1, column2) {
+      return list.filter(function (item) {
+        // 入力がない場合は全件表示
+        // return item[column] == value || value === "";
+        return item[column1] == value || item[column2] == value;
+      });
+    },
+  },
+  filters: {
+    capitalize: function (value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+  },
+  computed: {
+    messages_filtered() {
+      // return this.$store.state.messages;
+      return this.findBy(
+        this.$store.state.messages,
+        "masaki.hrak@gmail.com",
+        "sender",
+        "receiver"
+      );
+    },
+  },
 };
 </script>
 <style>
